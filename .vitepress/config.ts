b30fda0915e7a5bc6d6886bf0479b6b519b2ee97
cheetamaru <vitalvat@gmail.com>
2024-07-07 16:59:27 +0300

    feat: init

---
+++ .vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "IT-блог | Cheetamaru",
+  description: "IT-блог | Cheetamaru",
+  head: [
+    ['link', { rel: "shortcut icon", href: "/blog/favicon.ico"}],
+    ['meta', { name: "google", content: "notranslate"}],
+  ],
+  lang: "ru",
+  base: "/blog/",
+  cleanUrls: true,
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    lightModeSwitchTitle: "Темная тема",
+    darkModeSwitchTitle: "Светлая тема",
+    darkModeSwitchLabel: "Тема",
+    returnToTopLabel: "Наверх",
+
+    sidebar: [],
+
+    outline: {
+      label: "Содержание"
+    },
+  },
+  markdown: {
+    image: {
+      lazyLoading: true,
+    },
+    container: {
+      tipLabel: " "
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+pageClass: custom-page-class
+
+hero:
+  name: "IT-блог"
+  text: "Cheetamaru"
+  tagline: Тексты про IT
+  # actions:
+  #   - theme: brand
+  #     text: Markdown Examples
+  #     link: /markdown-examples
+  #   - theme: alt
+  #     text: API Examples
+  #     link: /api-examples
+
+features:
+  - title: "Blog"
+    details: ""
+    link: /discogs
+    icon: ⚡️
+---
